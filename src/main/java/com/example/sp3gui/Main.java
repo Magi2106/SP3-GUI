176333c2df5610c7981a0f4048d390833bdaf447
Magnewei <143735701+Magnewei@users.noreply.github.com>
2023-11-30 20:49:28 +0100

    Giga shrek is alive v2

--- src/main/java/com/example/sp3gui/Main.java
+++ src/main/java/com/example/sp3gui/Main.java
@@ -1,51 +1,11 @@
 package com.example.sp3gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.collections.ObservableList;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.collections.ObservableList;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-
-import static java.lang.System.exit;
-
-
-
 
 public class Main extends Application {
     MainController main = new MainController();
@@ -69,10 +29,6 @@ public class Main extends Application {
         primaryStage.setTitle("ShrekFlix.exe");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
-
-
 
 
     }
